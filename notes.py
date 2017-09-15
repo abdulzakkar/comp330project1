63ef3b4f0242275fd5488333eb1c6e1d1816170e
abdulzakkar <azakkar@luc.edu>
2017-09-15 18:55:11 +0000

    added regex functionality to notes.py and added sample notes.txt file.

--- notes.py
+++ notes.py
@@ -1,7 +1,14 @@
 import os
 import sys
+import re
 
 def main():
+	f = open('notes.txt', 'r')
+	s = f.readlines()
+	f.close()
 
+	for i in range(len(s)):
+		if re.search('^!', s[i]):
+			print s[i]
 
 main()
